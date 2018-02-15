53b6680d9a72b3f90785898155f7f4c093219114
Zaneta <Zaneta@ZANETA-PC>
2018-02-15 10:44:56 +0100

    Add project files.

---
+++ CandidatesBrowser2/Candidate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandidatesBrowser2
+{
+    class Candidate
+    {
+        public int ID { get; set; }
+        public  string FirstName { get; set; }
+    }
+}
